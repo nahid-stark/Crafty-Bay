7b5f69d5a0c9f0a3b36e2a3d12fe3039bdd52f47
nahid-stark <mminahid3@gmail.com>
2024-05-05 11:31:00 +0600

    Crafty Bay Project Share On Github

---
+++ README.md
@@ -0,0 +1,3 @@
+# E-Commerce Project by Flutter
+## Crafty Bay
+## Flutter Version Details Added on FVM Folder
---
+++ android/app/src/main/kotlin/com/ecommerce/crafty_bay/crafty_bay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ecommerce.crafty_bay.crafty_bay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
